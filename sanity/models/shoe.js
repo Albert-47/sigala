fd303549bb672a0d377a3c68ad2527351a204c05
Albert <albertquevedo35@gmail.com>
2023-05-09 19:16:27 -0400

    ADD added sanity integration to the project

---
+++ sanity/models/shoe.js
@@ -0,0 +1,19 @@
+const shoe = {
+    name: 'shoe',
+    type: 'document',
+    title: 'Shoe',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        }
+    ]
+};
+
+export default shoe;
